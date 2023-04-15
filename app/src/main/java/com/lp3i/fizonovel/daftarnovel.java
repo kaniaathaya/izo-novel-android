7165b3e420ba79b33f0720da6d37d9b44a900acc
Trisna1234 <dewirhmwt94@gmail.com>
2023-04-15 17:03:58 +0700

    daftar novel

---
+++ app/src/main/java/com/lp3i/fizonovel/daftarnovel.java
@@ -0,0 +1,14 @@
+package com.lp3i.fizonovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class daftarnovel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftarnovel);
+    }
+}
